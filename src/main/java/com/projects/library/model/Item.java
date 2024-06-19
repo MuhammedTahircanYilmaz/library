a18908f95ae0e38f3206c3d40f53c0bde4093300
MuhammedTahircanYilmaz <mtahircanyilmaz@gmail.com>
2024-06-19 16:23:49 +0300

    changed to class based entities and lombok is utilized

--- src/main/java/com/projects/library/model/Item.java
+++ src/main/java/com/projects/library/model/Item.java
@@ -3,51 +3,41 @@ package com.projects.library.model;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 
-
-import java.beans.ConstructorProperties;
 import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-import static com.projects.library.model.Status.ONGOING;
-import static com.projects.library.model.Type.ANIME;
 
 @Entity
-public record Item(
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Item {
         @Id
         @GeneratedValue(strategy = GenerationType.IDENTITY)
-        Long id,
+        private Long id;
         @Column(name = "title")
         @NotBlank
-        String title,
+        private String title;
         @Column(name = "description")
-        String description,
+        private String description;
         @Enumerated(EnumType.STRING)
         @NotBlank
-        Type itemType,
+        private Type itemType;
         @Column(name = "author_studio")
         @NotBlank
-        String authorStudio,
+        private String authorStudio;
         @Column(name = "rating")
         @NotBlank
-        Integer rating,
+        private Integer rating;
         @Enumerated(EnumType.STRING)
         @NotBlank
-        Status status,
+        private Status status;
         @Column(name = "date_created")
-        LocalDateTime dateCreated,
+        private LocalDateTime dateCreated;
         @Column(name = "date_updated")
-        LocalDateTime dateUpdated
-){
-        @ConstructorProperties({"id", "title", "description", "itemType","authorStudio",
-                                "rating", "status", "dateCreated","dateUpdated"})
-        public Item{
-
-        }
-        public Item(){
-                this(null,"name","some description",ANIME,"author",
-                        3,ONGOING, LocalDateTime.now(), LocalDateTime.now());
-        }
-
-        public Long getId(){
-                return id;
-        }
+        private LocalDateTime dateUpdated;
 }
