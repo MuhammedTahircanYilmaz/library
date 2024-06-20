3169f829a21a59b29793e8307b760cabd5777010
MuhammedTahircanYilmaz <mtahircanyilmaz@gmail.com>
2024-06-20 10:24:50 +0300

    User service and impl completed, UserEntityDto created

--- src/main/java/com/projects/library/service/UserService.java
+++ src/main/java/com/projects/library/service/UserService.java
@@ -1,4 +1,12 @@
 package com.projects.library.service;
 
+import com.projects.library.dto.UserEntityDto;
+import com.projects.library.model.UserEntity;
+import org.apache.catalina.User;
+
 public interface UserService {
+
+    void save(UserEntityDto userEntityDto);
+
+    UserEntity findUserByEmail(String email);
 }
