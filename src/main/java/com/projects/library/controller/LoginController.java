d749d2fa0d308e1cebc2c6a19da7ac899c67afd7
MuhammedTahircanYilmaz <mtahircanyilmaz@gmail.com>
2024-06-20 17:45:35 +0300

    Login Controller completed

--- src/main/java/com/projects/library/controller/LoginController.java
+++ src/main/java/com/projects/library/controller/LoginController.java
@@ -1,7 +1,19 @@
 package com.projects.library.controller;
 
+import com.projects.library.dto.UserEntityDto;
 import org.springframework.stereotype.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.ui.Model;
 
 @Controller
 public class LoginController {
+    @GetMapping("")
+    public String login(Model model){
+        UserEntityDto user = new UserEntityDto();
+        model.addAttribute("userEntity", user);
+        return "login";
+    }
+
 }
--- src/main/java/com/projects/library/service/UserServiceImpl.java
+++ src/main/java/com/projects/library/service/UserServiceImpl.java
@@ -2,26 +2,22 @@ package com.projects.library.service;
 
 import com.projects.library.dto.UserEntityDto;
 import com.projects.library.model.UserEntity;
-import com.projects.library.repository.UserRepository;
-import org.apache.catalina.User;
+import com.projects.library.repository.UserEntityRepository;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService{
 
-    private UserRepository userRepository;
+    private UserEntityRepository userRepository;
     private BCryptPasswordEncoder bCryptPasswordEncoder;
 
-    public UserServiceImpl(UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder){
-        this.userRepository = userRepository;
-        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
-    }
+
 
     @Override
     public void save(UserEntityDto userEntityDto) {
         UserEntity userEntity = new UserEntity();
-        userEntity.setUsername(userEntityDto.getUserName());
+        userEntity.setUsername(userEntityDto.getUsername());
         userEntity.setEmail(userEntityDto.getEmail());
         String hashedPassword = bCryptPasswordEncoder.encode(userEntityDto.getPassword());
         userEntity.setPassword(hashedPassword);
